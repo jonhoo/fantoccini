12e3bf8fe9e48cfbdc156a09992786175bdd0b4d
lazypassion <25536767+lazypassion@users.noreply.github.com>
2019-08-27 11:42:22 -0400

    removed some commented nonworking code in sync example

--- examples/sync_example.rs
+++ examples/sync_example.rs
@@ -2,14 +2,7 @@ use fantoccini::error::CmdError;
 use fantoccini::sync::{Client};
 use std::error::Error;
 
-// fn navigate_rustlang<T>() -> Result<T, Box<dyn Error>> {
-//     let client = Client::new("http://localhost:4444")?;
-
-//     client.goto("https://www.rust-lang.org/")
-// }
-// fn main() -> Result<T, Box<dyn Error>> {
 fn main() {
-    // navigate_rustlang().unwrap();
     let mut client = Client::new("http://localhost:4444").unwrap();
 
     client.goto("https://www.rust-lang.org/").unwrap();
