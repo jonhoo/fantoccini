6ccfe4a9ea510756260e8dce963436bceecb6111
Jon Gjengset <jon@thesquareplanet.com>
2019-12-20 10:44:51 -0500

    Fix doctest

--- src/lib.rs
+++ src/lib.rs
@@ -102,7 +102,7 @@
 //!
 //! // we then read out the image bytes
 //! use futures_util::TryStreamExt;
-//! let pixels = raw.into_body().try_concat().await.map_err(fantoccini::error::CmdError::from)?;
+//! let pixels = hyper::body::to_bytes(raw.into_body()).await.map_err(fantoccini::error::CmdError::from)?;
 //! // and voilla, we now have the bytes for the Wikipedia logo!
 //! assert!(pixels.len() > 0);
 //! println!("Wikipedia logo is {}b", pixels.len());
