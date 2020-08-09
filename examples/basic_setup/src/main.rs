6eeacb8a44c349d77713cbf5665251b3d8d78d47
Yerkebulan Tulibergenov <yerkebulan@gmail.com>
2020-08-08 21:12:45 -0700

    add delay_for as in the old example

--- examples/basic_setup/src/main.rs
+++ examples/basic_setup/src/main.rs
@@ -1,4 +1,6 @@
 use fantoccini::{Client, Locator};
+use std::time::Duration;
+use tokio::time::delay_for;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -8,6 +10,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .expect("failed to connect to WebDriver");
 
     client.goto("https://www.rust-lang.org/").await?;
+    // delay_for is an artificial delay only used to see the browsers actions and not necessary
+    // in your own code
+    delay_for(Duration::from_millis(3000)).await;
 
     let get_started_button =
         r#"//a[@class="button button-download ph4 mt0 w-100" and @href="/learn/get-started"]"#;
@@ -15,6 +20,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .wait_for_find(Locator::XPath(get_started_button))
         .await?;
     element.click().await?;
+    delay_for(Duration::from_millis(3000)).await;
 
     let try_without_installing_button =
         r#"//a[@class="button button-secondary" and @href="https://play.rust-lang.org/"]"#;
@@ -22,12 +28,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .wait_for_find(Locator::XPath(try_without_installing_button))
         .await?;
     element.click().await?;
+    delay_for(Duration::from_millis(3000)).await;
 
     let play_rust_lang_run_button = r#"//div[@class="segmented-button"]/button[1]"#;
     let element = client
         .wait_for_find(Locator::XPath(play_rust_lang_run_button))
         .await?;
     element.click().await?;
+    delay_for(Duration::from_millis(6000)).await;
 
     Ok(())
 }
