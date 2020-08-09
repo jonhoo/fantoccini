d4a31d50f7d5e4500f4c6a3eae0a6cfdc7a49e22
Yerkebulan Tulibergenov <yerkebulan@gmail.com>
2020-08-09 02:16:20 -0700

    replace wait_for_find() with find()

--- examples/basic_setup/src/main.rs
+++ examples/basic_setup/src/main.rs
@@ -16,23 +16,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let get_started_button =
         r#"//a[@class="button button-download ph4 mt0 w-100" and @href="/learn/get-started"]"#;
-    let element = client
-        .wait_for_find(Locator::XPath(get_started_button))
-        .await?;
+    let element = client.find(Locator::XPath(get_started_button)).await?;
     element.click().await?;
     delay_for(Duration::from_millis(3000)).await;
 
     let try_without_installing_button =
         r#"//a[@class="button button-secondary" and @href="https://play.rust-lang.org/"]"#;
     let element = client
-        .wait_for_find(Locator::XPath(try_without_installing_button))
+        .find(Locator::XPath(try_without_installing_button))
         .await?;
     element.click().await?;
     delay_for(Duration::from_millis(3000)).await;
 
     let play_rust_lang_run_button = r#"//div[@class="segmented-button"]/button[1]"#;
     let element = client
-        .wait_for_find(Locator::XPath(play_rust_lang_run_button))
+        .find(Locator::XPath(play_rust_lang_run_button))
         .await?;
     element.click().await?;
     delay_for(Duration::from_millis(6000)).await;
