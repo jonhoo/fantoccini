b1d1cadcf745817e7bc27ca231fce7f6ed0762b4
Yerkebulan Tulibergenov <yerkebulan@gmail.com>
2020-08-10 05:52:12 -0700

    Update basic_setup example to use async await syntax (#101)

--- examples/basic_setup/src/main.rs
+++ examples/basic_setup/src/main.rs
@@ -1,46 +1,39 @@
-use fantoccini::error::CmdError;
 use fantoccini::{Client, Locator};
-use futures::future::Future;
-use tokio;
+use std::time::Duration;
+use tokio::time::delay_for;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // expects WebDriver instance to be listening at port 4444
-    let client = Client::new("http://localhost:4444");
+    let mut client = Client::new("http://localhost:4444")
+        .await
+        .expect("failed to connect to WebDriver");
+
+    client.goto("https://www.rust-lang.org/").await?;
+    // delay_for is an artificial delay only used to see the browsers actions and not necessary
+    // in your own code
+    delay_for(Duration::from_millis(3000)).await;
+
     let get_started_button =
         r#"//a[@class="button button-download ph4 mt0 w-100" and @href="/learn/get-started"]"#;
+    let element = client.find(Locator::XPath(get_started_button)).await?;
+    element.click().await?;
+    delay_for(Duration::from_millis(3000)).await;
+
     let try_without_installing_button =
         r#"//a[@class="button button-secondary" and @href="https://play.rust-lang.org/"]"#;
+    let element = client
+        .find(Locator::XPath(try_without_installing_button))
+        .await?;
+    element.click().await?;
+    delay_for(Duration::from_millis(3000)).await;
+
     let play_rust_lang_run_button = r#"//div[@class="segmented-button"]/button[1]"#;
+    let element = client
+        .find(Locator::XPath(play_rust_lang_run_button))
+        .await?;
+    element.click().await?;
+    delay_for(Duration::from_millis(6000)).await;
 
-    let rust_lang = client
-        .map_err(|error| unimplemented!("failed to connect to WebDriver: {:?}", error))
-        .and_then(|client| client.goto("https://www.rust-lang.org/"))
-        // client_wait is an artificial delay only used to see the browsers actions and not necessary
-        // in your own code
-        .and_then(|client| client_wait(client, 3000))
-        .and_then(move |client| client.wait_for_find(Locator::XPath(get_started_button)))
-        .and_then(|element| element.click())
-        .and_then(|client| client_wait(client, 3000))
-        .and_then(move |client| client.wait_for_find(Locator::XPath(try_without_installing_button)))
-        .and_then(|element| element.click())
-        .and_then(|client| client_wait(client, 3000))
-        .and_then(move |client| {
-            client.wait_for_find(Locator::XPath(play_rust_lang_run_button))
-        })
-        .and_then(|element| element.click())
-        .and_then(|client| client_wait(client, 6000))
-        .map(|_| ())
-        .map_err(|error| panic!("a WebDriver command failed: {:?}", error));
-
-    tokio::run(rust_lang);
-}
-
-// helper function to delay the client
-fn client_wait(client: Client, delay: u64) -> impl Future<Item = Client, Error = CmdError> {
-    use std::time::{Duration, Instant};
-    use tokio::timer::Delay;
-
-    Delay::new(Instant::now() + Duration::from_millis(delay))
-        .and_then(|_| Ok(client))
-        .map_err(|error| panic!("client failed to wait with error: {:?}", error))
+    Ok(())
 }
