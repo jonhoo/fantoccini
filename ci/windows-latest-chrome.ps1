0b37bd99da3579e9d2c4801c551291caf3398a5e
Jon Gjengset <jon@thesquareplanet.com>
2024-07-14 11:38:14 +0200

    Upgrade firefox and chrome (to chrome for testing)

--- ci/windows-latest-chrome.ps1
+++ ci/windows-latest-chrome.ps1
@@ -1,4 +1,5 @@
-choco install googlechrome
-choco install chromedriver
+npm i puppeteer
+npx @puppeteer/browsers install chrome@stable
+npx @puppeteer/browsers install chromedriver@stable
 Start-Process -FilePath chromedriver
 Start-Sleep -Seconds 1
--- ci/windows-latest-firefox.ps1
+++ ci/windows-latest-firefox.ps1
@@ -1,5 +1,5 @@
 choco install firefox
-Invoke-WebRequest -Uri "https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-win64.zip" -OutFile geckodriver.zip
+Invoke-WebRequest -Uri "https://github.com/mozilla/geckodriver/releases/download/v0.34.0/geckodriver-v0.34.0-win64.zip" -OutFile geckodriver.zip
 Expand-Archive -LiteralPath geckodriver.zip -DestinationPath .
 Start-Process -FilePath geckodriver
 Start-Sleep -Seconds 1
