0974baa3f6aca729caba25d632966bd0ccf45d8f
Jon Gjengset <jon@thesquareplanet.com>
2024-07-14 12:50:04 +0200

    Back to stable again

--- ci/windows-latest-chrome.ps1
+++ ci/windows-latest-chrome.ps1
@@ -1,5 +1,5 @@
 npm i puppeteer
-npx @puppeteer/browsers install chrome@125
-npx @puppeteer/browsers install chromedriver@125
+npx @puppeteer/browsers install chrome@stable
+npx @puppeteer/browsers install chromedriver@stable
 Start-Process -FilePath chromedriver
 Start-Sleep -Seconds 1
