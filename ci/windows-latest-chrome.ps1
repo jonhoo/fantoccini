a10e6fc2089bc7a3c876fdc83bb990c9777356c4
David Hewson <david.hewson@tracsis.com>
2024-07-15 12:41:06 +0100

    chromedriver flags that make ci happy eveywhere

--- ci/windows-latest-chrome.ps1
+++ ci/windows-latest-chrome.ps1
@@ -1,6 +1,5 @@
 npm i puppeteer
-# pin to 125 due to https://issues.chromium.org/issues/42323434
-npx @puppeteer/browsers install chrome@125
-npx @puppeteer/browsers install chromedriver@125
+npx @puppeteer/browsers install chrome@stable
+npx @puppeteer/browsers install chromedriver@stable
 Start-Process -FilePath chromedriver
 Start-Sleep -Seconds 1
